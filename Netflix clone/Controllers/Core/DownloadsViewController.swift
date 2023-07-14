7cb771b47696bfccca984f67a020e3af22efb861
Anas Riahi <anasriahi.dev@gmail.com>
2023-07-14 15:37:56 +0100

    Using Notification Center to update ViewControllers

--- Netflix clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix clone/Controllers/Core/DownloadsViewController.swift
@@ -29,6 +29,10 @@ class DownloadsViewController: UIViewController {
         downloadsTable.dataSource = self
         
         fetchLocalStorageForDownload()
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("Downloaded"), object: nil, queue: nil) { _ in
+            self.fetchLocalStorageForDownload()
+        }
     }
     
     private func fetchLocalStorageForDownload() {
@@ -89,6 +93,28 @@ extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
             }
         default:
             break;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        
+        guard let titleName = title.original_name ?? title.original_title else { return }
+        
+        APICaller.shared.getMovieFromYoutube(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
 }
